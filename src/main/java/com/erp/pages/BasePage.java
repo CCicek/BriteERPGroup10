d2980653d2c2616601397e63f74a90e2dbaba092
vatanhorasan <vatanhorasan@gmail.com>
2020-06-19 13:35:14 -0500

    update BasePage.java and add InvoicingPage.java

--- src/main/java/com/erp/pages/BasePage.java
+++ src/main/java/com/erp/pages/BasePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePage {
     public BasePage() {
@@ -14,6 +16,9 @@ public abstract class BasePage {
     @FindBy(xpath = "//div[@class='o_loading']")
     public WebElement loadingDisplay;
 
+    @FindBy(css = ".breadcrumb>li")
+    public WebElement pageSubTitle;
+
 
 
 
@@ -21,4 +26,16 @@ public abstract class BasePage {
     public void navigateTo(String modul) {
         Driver.get().findElement(By.xpath("//span[contains(text(),'" + modul + "')]")).click();
     }
+
+
+    public String getPageSubTitle(){return pageSubTitle.getText();}
+
+    public void waitTheTitleIs(String title){
+        new WebDriverWait(Driver.get(),10).until(ExpectedConditions.titleIs(title));
+    }
+
+    public void waitTheTitleContains(String title){
+        new WebDriverWait(Driver.get(),10).until(ExpectedConditions.titleContains(title));
+    }
+
 }
