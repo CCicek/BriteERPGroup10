c5b2dcd6bc1112ed65a019484cbafa2ba3ec83f2
TARIKERKAM <tedogan@hotmail.com>
2020-06-23 09:33:18 +0400

    test1

--- src/test/java/com/erp/pages/CRMPage.java
+++ src/test/java/com/erp/pages/CRMPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class CRMPage extends BasePage {
-    //create button
+    //create button enter
     @FindBy(xpath = "(//button[@class='btn btn-primary btn-sm o-kanban-button-new'])")
     public WebElement create;
 
