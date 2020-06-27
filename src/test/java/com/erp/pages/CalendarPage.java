d9b4eaf0724c5586825521a706b2aa647e4cf42e
memocpr <memocpr35@gmail.com>
2020-06-27 21:30:34 +0200

    calenderr

--- src/test/java/com/erp/pages/CalendarPage.java
+++ src/test/java/com/erp/pages/CalendarPage.java
@@ -1,6 +1,24 @@
 package com.erp.pages;
 
+import com.erp.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class CalendarPage extends BasePage {
+
+    @FindBy(css = "th.fc-day-header.fc-widget-header")
+    public List<WebElement> daysOfWeek;
+
+
+    public void calenderRange(String range){
+
+        String rangePath="button.o_calendar_button_"+range;
+        Driver.get().findElement(By.cssSelector(rangePath)).click();
+    }
+
 
 
 }
